4057a7b112d84e3980e8b22c526c1352cb93365a
rnascunha85 <rnascunha@gmail.com>
2021-06-15 23:17:32 -0300

    Change ESP build

--- esp_idf.cmake
+++ esp_idf.cmake
@@ -2,7 +2,7 @@ include (${COMPONENT_DIR}/files.cmake)
 
 list(APPEND MAIN_SRC ${SRC_DIR_PORT}/c_standard_port.cpp)
 
-idf_component_register(SRCS ${MAIN_SRC}
+idf_component_register(SRCS ${MAIN_SRC} ${SRC_PORT_POSIX} ${SRC_PORT_ESP_MESH}
                        INCLUDE_DIRS ${MAIN_INCLUDE_DIRS}
                        REQUIRES)
 
@@ -28,6 +28,7 @@ target_compile_options(${COMPONENT_LIB}
 							-DCOAP_TE_ESP_IDF_PLATAFORM=1 
 							-DCOAP_TE_PORT_C_STANDARD=1
 							-DCOAP_TE_PORT_POSIX=1
+							-DCOAP_TE_PORT_ESP_MESH=1
 							-DCOAP_TE_USE_SELECT=1
 							-DCOAP_TE_USE_ERROR_MESSAGES=${CONFIG_COAP_TE_MESSAGE_ERROR}
 							-DCOAP_TE_LOG_COLOR=${CONFIG_COAP_TE_USE_COLOR}
